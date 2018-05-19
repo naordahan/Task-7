d8c320e62a799ef3e5ec250bff0d12546a934f09
naordahan <33223417+naordahan@users.noreply.github.com>
2018-05-19 23:11:39 +0300

    Update Coordinate.cpp

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 Coordinate::Coordinate(int x2,int y2){
         this->x=x2;
-        this->sy=y2;
+        this->y=y2;
 }
 ostream& operator<< (ostream& os, const Coordinate& p){
          os<<"("+to_string(p.x)+","+to_string(p.y)+")";
